a1ba1f52c596d892d7a8461aea9817d8540d5952
Mushvig Shukurov <musfiqsukurov9@gmail.com>
2025-11-03 16:38:18 +0400

    CRUD+Completed

--- mymodels.py
+++ mymodels.py
@@ -3,4 +3,4 @@ from tortoise import models, fields
 class Todo(models.Model):
     id = fields.IntField(primary_key=True)
     description = fields.TextField()
-    is_active = fields.BooleanField(default=False)
+    is_active = fields.BooleanField(default=True)
