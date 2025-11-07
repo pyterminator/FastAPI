fbf2a22accf178fbcaf710fd05681b9b1a518417
Mushvig Shukurov <musfiqsukurov9@gmail.com>
2025-11-07 16:02:33 +0400

    -LS10

---
+++ tutorials/video_10.py
@@ -0,0 +1,10 @@
+# from passlib.context import CryptContext
+
+# password_context = CryptContext(schemes=['argon2'], deprecated="auto")
+
+# my_password = "12345"
+
+# my_hashed_password = password_context.hash(my_password)
+
+# print(my_password)
+# print(my_hashed_password)
